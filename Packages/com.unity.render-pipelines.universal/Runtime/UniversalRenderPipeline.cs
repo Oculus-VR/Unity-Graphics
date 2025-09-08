42d5232561848971ca43d3862e34437cc7d3e89e
Trevor Dasch <trevor.dasch@meta.com>
2025-09-08 17:23:31 -0400

    Use memoryless MSAA intermediate buffers if possible

--- Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/UniversalRenderPipeline.cs
@@ -913,6 +913,7 @@ namespace UnityEngine.Rendering.Universal
                 baseCameraData.cameraTargetDescriptor.graphicsFormat = originalTargetDesc.graphicsFormat;
             }
             baseCameraData.cameraTargetDescriptor.msaaSamples = originalTargetDesc.msaaSamples;
+            baseCameraData.cameraTargetDescriptor.memoryless = originalTargetDesc.memoryless;
 
             if (baseCameraData.isDefaultViewport)
             {
