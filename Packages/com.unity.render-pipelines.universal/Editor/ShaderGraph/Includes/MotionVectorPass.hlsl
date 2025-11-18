e27021e0b96bb8fd71bc7e141496d8e4b19d4850
Trevor Dasch <trevor.dasch@meta.com>
2025-11-18 12:47:50 -0500

    Replicate particle system workaround from ObjectMotionVectors.hlsl in MotionVectorPass.hlsl to fix ShaderGraph particle effects

--- Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/MotionVectorPass.hlsl
+++ Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/MotionVectorPass.hlsl
@@ -66,6 +66,15 @@ float3 GetLastFrameDeformedPosition(Attributes input, MotionVectorPassOutput cur
     return previousPositionOS;
 }
 
+bool IsIdentity(in float4x4 modelMatrix)
+{
+    return
+        modelMatrix[0][0] == 1.0 && modelMatrix[0][1] == 0.0 && modelMatrix[0][2] == 0.0 && modelMatrix[0][3] == 0.0 &&
+        modelMatrix[1][0] == 0.0 && modelMatrix[1][1] == 1.0 && modelMatrix[1][2] == 0.0 && modelMatrix[1][3] == 0.0 &&
+        modelMatrix[2][0] == 0.0 && modelMatrix[2][1] == 0.0 && modelMatrix[2][2] == 1.0 && modelMatrix[2][3] == 0.0 &&
+        modelMatrix[3][0] == 0.0 && modelMatrix[3][1] == 0.0 && modelMatrix[3][2] == 0.0 && modelMatrix[3][3] == 1.0;
+}
+
 // -------------------------------------
 // Vertex
 void vert(
@@ -150,7 +159,18 @@ void vert(
         mvOutput.previousPositionCSNoJitter = mul(_PrevViewProjMatrix, float4(previousPositionWS, 1.0f));
     #endif
 #else
-        mvOutput.previousPositionCSNoJitter = mul(_PrevViewProjMatrix, mul(UNITY_PREV_MATRIX_M, float4(previousPositionOS, 1.0f)));
+        // Particle System Workaround.
+        // There is currently a bug in Unity that UNITY_PREV_MATRIX_M is relative to the particle system
+        // transform, but UNITY_MATRIX_M is always identity, causing artifacts for particles with motion vectors.
+        // We can avoid this bug by checking whether the current model matrix is the identity matrix, and if
+        // so, simply use the unaltered previous position without multiplying by UNITY_PREV_MATRIX_M.
+        const float3 prevPositionWS = previousPositionOS;
+        if (!IsIdentity(UNITY_MATRIX_M))
+        {
+            prevPositionWS = mul(UNITY_PREV_MATRIX_M, float4(previousPositionOS, 1.0f)).xyz;
+        }
+
+        mvOutput.previousPositionCSNoJitter = mul(_PrevViewProjMatrix, float4(prevPositionWS, 1.0f));
 #endif
     }
 
